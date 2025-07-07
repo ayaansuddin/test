53cf691b1f0b9541aea1f313299e1e5d58e07242
Ayaan Uddin <ayaansuddin@gmail.com>
2025-07-07 11:19:38 -0500

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Ayaan Uddin on 7/7/25.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
